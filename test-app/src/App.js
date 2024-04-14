56545d635c54750325aede2a398851fd9db8fd1d
cxzaqq <roy1228@naver.com>
2024-04-15 01:14:28 +0900

    stepper

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,10 +1,70 @@
-import ProgressMobileStepper from "./components/stepper";
+import * as React from "react";
+import MobileStepper from "@mui/material/MobileStepper";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 function App() {
+  const totalStep = 10;
+
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => {
+      if (prevActiveStep < totalStep - 1) return prevActiveStep + 1;
+      else return totalStep - 1;
+    });
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => {
+      if (prevActiveStep > 0) return prevActiveStep - 1;
+      else return 0;
+    });
+  };
+
   return (
-    <div>
-      <ProgressMobileStepper></ProgressMobileStepper>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "96vh",
+        justifyContent: "space-between",
+        mt: 1,
+      }}
+    >
+      <Box sx={{ mt: 2 }}>
+        <MobileStepper
+          variant="dots"
+          steps={totalStep}
+          position="static"
+          activeStep={activeStep}
+          sx={{ maxWidth: 400, margin: "auto" }}
+        />
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        {activeStep !== 0 && ( // 0단계가 아닐 때만 이전 버튼을 표시
+          <Button
+            variant="contained"
+            size="large"
+            onClick={handleBack}
+            sx={{ mr: 1 }}
+          >
+            이전
+          </Button>
+        )}
+        {activeStep !== totalStep - 1 && (
+          <Button variant="contained" size="large" onClick={handleNext}>
+            다음
+          </Button>
+        )}
+        {activeStep === totalStep - 1 && (
+          <Button variant="contained" size="large" onClick={handleNext}>
+            제출
+          </Button>
+        )}
+      </Box>
+    </Box>
   );
 }
 
